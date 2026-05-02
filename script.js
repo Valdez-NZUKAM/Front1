8ca58759b37d94eb1d5907e2d6c3624c32e1f65a
Lukario Valdez <nzukam.yvan@institutsaintjean.org>
2026-05-02 23:25:36 +0200

    1st script js

---
+++ script.js
@@ -0,0 +1,5 @@
+// console.log("Hello, World!"); // pour le display dans la console du navigateur
+
+// window.alert("Bienvenue sur notre site de formation en ligne !"); // pour afficher une alerte de bienvenue
+
+document.getElementById("myH1").textContent = "Bienvenue sur notre site de formation en ligne Hello!"; // pour changer le texte d'un élément HTML avec l'id "myH1"
